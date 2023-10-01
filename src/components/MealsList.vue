d194ce028797ff2269aadfb8f98f307992637141
dhollifield <deannahollifield@gmail.com>
2023-10-01 16:43:58 -0500

    added user list and meal list

---
+++ src/components/MealsList.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="child">
+        <h1>Meals</h1>
+        <div v-for="meal in meals" :key="meal.id" class="meals">
+                <p class="loop-table">MAIN DISH: {{ meal.mainDish }}</p>
+                <p class="loop-table">SIDE DISH: {{ meal.sideDish1 }}</p>
+                <p class="loop-table">SIDE DISH: {{ meal.sideDish2 }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            meals: []
+        }
+    },
+    methods: {
+        getMeals() {
+            fetch('https://localhost:7026/api/Meal')
+                .then(response => response.json())
+                .then(data => this.meals = data)
+        }
+    },
+    mounted() {
+        this.getMeals()
+    }
+}
+</script>
+
+<style scoped>
+.child {
+    background-color: #f4a261;
+    padding: 30px;
+    margin: 20px;
+}
+
+.meals {
+    background-color: whitesmoke;
+    border: 2px solid black;
+    padding: 10px;
+}
+
+</style>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="child">
+        <h1>Users</h1>
+        <div v-for="user in users" :key="user.id" class="users">
+                <p class="loop-table">NAME: {{ user.firstName }} {{  user.lastName }}</p>
+                <p class="loop-table">EMAIL: {{ user.email }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            users: []
+        }
+    },
+    methods: {
+        getUsers() {
+            fetch('https://localhost:7026/api/User')
+                .then(response => response.json())
+                .then(data => this.users = data)
+        }
+    }, 
+    mounted() {
+        this.getUsers()
+    }
+}
+</script>
+
+<style scoped>
+.child {
+    background-color: #e9c46a;
+    padding: 30px;
+    margin: 20px;
+}
+
+.users {
+    background-color: whitesmoke;
+    border: 2px solid black;
+    padding: 10px;
+}
+
+</style>
